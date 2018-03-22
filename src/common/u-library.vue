77a2df51b809126800d1b499b1ae7ab271010f48
hzshaoyuee1 <hzshaoyuee1@corp.netease.com>
2018-03-22 15:26:52 +0800

    update libraries UI

--- src/common/u-library.vue
+++ src/common/u-library.vue
@@ -1,16 +1,23 @@
 <template>
-<a :class="$style.root" :more="more">
-    <template v-if="!more">
-        <div :class="$style.head">
-            <div v-if="logo" :class="$style.logo" :name="logo"></div>
-            <div :class="$style.title">{{ title }}</div>
-        </div>
-        <div :class="$style.colors">
-            <span v-for="color in colors" :key="color" :class="$style.color" :style="{ background: color }"></span>
-        </div>
-    </template>
-    <div v-else :class="$style.more" title="我们想提供一套自己的组件库"></div>
-</a>
+    <a :class="$style.root" :more="more">
+        <template v-if="!more">
+            <div :class="$style.head">
+                <div :class="$style.logo" :name="logo" :style="{background: defaultLogo ? logoColor : ''}">{{ defaultLogo }}</div>
+                <div :class="$style.title">{{ title }}</div>
+            </div>
+            <div :class="$style.body">
+                <slot></slot>
+            </div>
+            <div :class="$style.footer">
+                <div :class="$style.brief">{{ brief }}</div>
+                <div v-if="colors" :class="$style.colors">
+                    颜色：
+                    <span v-for="color in colors" :key="color" :class="$style.color" :style="{ background: color }"></span>
+                </div>
+            </div>
+        </template>
+        <div v-else :class="$style.more" title="我们想提供一套自己的组件库"></div>
+    </a>
 </template>
 
 <script>
@@ -18,83 +25,110 @@ export default {
     name: 'u-library',
     props: {
         title: String,
-        logo: String,
+        logo: { type: String, default: 'default' },
+        logoColor: { type: String, default: '#1ba67e' },
+        brief: String,
         colors: Array,
         more: { type: Boolean, default: false },
+    },
+    computed: {
+        defaultLogo() {
+            return this.logo === 'default' ? this.title.substr(0, 1) : '';
+        },
     },
 };
 </script>
 
 <style module>
 .root {
+    position: relative;
     display: block;
-    width: 200px;
-    height: 200px;
+    width: 270px;
+    height: 185px;
+    box-sizing: border-box;
+    padding: 20px;
     background: white;
     box-shadow: 0 0 2px 0 rgba(80,90,109,0.16);
-    position: relative;
     cursor: default;
-}
-
-.root[href]:hover {
-    box-shadow: 0 0 10px 0 rgba(80,90,109,0.16);
-}
-
-.logo[name="163yun"] {
-    display: inline-block;
-    margin-top: 40px;
-    margin-bottom: -20px;
-    width: 130px;
-    height: 80px;
-    background: url(../assets/logo-163yun-colorful.svg) no-repeat;
-    background-size: 100% 100%;
-}
-
-.head {
-    height: 160px;
-    font-size: 22px;
-    text-align: center;
-}
-
-.title {
-    color: #d5dce4;
-    line-height: 180px;
-}
-
-.logo + .title {
-    line-height: 1em;
-    font-size: 18px;
 }
 
 .root[href] {
     cursor: pointer;
 }
 
-.root[href] .title {
-    color: #657f98;
+.root[href]:hover {
+    box-shadow: 0 0 10px 0 rgba(80,90,109,0.16);
+}
+
+.head {
+    height: 40px;
+}
+
+.logo[name="default"] {
+    display: inline-block;
+    width: 40px;
+    height: 40px;
+    text-align: center;
+    line-height: 40px;
+    border-radius: 50%;
+    color: #fff;
+    font-size: 16px;
+}
+
+.logo[name="163yun"] {
+    display: inline-block;
+    width: 50px;
+    height: 34px;
+    margin-top: -5px;
+    vertical-align: middle;
+    background: url(../assets/logo-cloud.png) no-repeat;
+    background-size: 100% 100%;
+}
+
+.title {
+    display: inline-block;
+    margin-left: 10px;
+    line-height: 40px;
+    font-size: 20px;
+    color: #2c3e50;
+}
+
+.body {
+    margin: 10px 0 15px;
+    height: 60px;
+    color: #616e7c;
+    font-size: 14px;
+    line-height: 20px;
+}
+
+.footer {
+    height: 20px;
+    color: #999;
+}
+
+.brief {
+    float: left;
 }
 
 .colors {
-    display: flex;
-    position: absolute;
-    bottom: 20px;
-    left: 20px;
+    float: right;
 }
 
 .color {
-    width: 1em;
-    height: 1em;
-    margin-right: 2px;
+    display: inline-block;
+    vertical-align: middle;
+    width: 12px;
+    height: 12px;
+    margin-right: 5px;
+    border-radius: 2px;
 }
 
 .more {
-    font-size: 72px;
-    text-align: center;
-    line-height: 200px;
-    color: #657f98;
-}
-
-.more:before {
-    icon-font: url('../assets/ic_add_black_24px.svg');
+    height: 32px;
+    width: 32px;
+    background: url(../assets/icon-add.png) no-repeat;
+    background-size: 32px;
+    margin-top: 56px;
+    margin-left: 100px;
 }
 </style>
--- src/views/libraries.vue
+++ src/views/libraries.vue
@@ -1,12 +1,17 @@
 <template>
     <div :class="$style.root">
-        <u-library title="Proto UI" :colors="['#1a9e78', '#ddd']" href="/proto-ui"></u-library>
-        <u-library target="_blank" title="Cloud Design" logo="163yun" :colors="['#67aaf5', '#4a89e8', '#2cb78e']" href="https://vusion.github.io/cloud-ui"></u-library>
+        <u-library :colors="['#1a9e78', '#ddd']" title="Proto UI" href="/proto-ui" brief="中文手册">
+            Proto UI 是一整套前端 UI 组件库，是快速、可靠、安全的依赖管理工具。
+        </u-library>
+        <u-library :colors="['#67aaf5', '#4a89e8', '#2cb78e']" target="_blank" title="Cloud Design" logo="163yun"
+                   href="https://vusion.github.io/cloud-ui">
+            Cloud Design 极速、零配置的 web 应用打包工具。
+        </u-library>
         <u-library target="_blank" title="Vusion UI"></u-library>
-        <u-library target="_blank" title="NetEase UI"></u-library>
-        <u-library target="_blank" title="Material Design"></u-library>
-        <u-library target="_blank" title="Flat UI"></u-library>
-        <u-library target="_blank" title="Mac Style" :colors="['#419bf9', '#f4f4f4']" href="https://vusion.github.io/mac-style"></u-library>
+        <u-library target="_blank" title="NetEase UI" logo-color="#e24244"></u-library>
+        <u-library target="_blank" title="Material Design" logo-color="#20bcd2"></u-library>
+        <u-library target="_blank" title="Flat UI" logo-color="#2abb9c"></u-library>
+        <u-library :colors="['#419bf9', '#f4f4f4']" target="_blank" title="Mac Style" logo-color="#459df6" href="https://vusion.github.io/mac-style"></u-library>
         <u-library more href="mailto:rainforest92@126.com?subject=%E6%88%91%E4%BB%AC%E6%83%B3%E6%8F%90%E4%BE%9B%E4%B8%80%E5%A5%97%E8%87%AA%E5%B7%B1%E7%9A%84%E7%BB%84%E4%BB%B6%E5%BA%93"></u-library>
     </div>
 </template>
@@ -29,6 +34,6 @@ export default {
 }
 
 .root > * {
-    margin: 20px;
+    margin: 15px;
 }
 </style>
